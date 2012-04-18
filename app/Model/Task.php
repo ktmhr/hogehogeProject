05efdb3c543fa8e194f1a6b09be4ae646c53ce9a
ktmhr <hifumi123@yahoo.co.jp>
2012-04-18 15:48:15 +0900

    add TaskModel

---
+++ app/Model/Task.php
@@ -0,0 +1,14 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Task Model
+ *
+ */
+class Task extends AppModel {
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+}
